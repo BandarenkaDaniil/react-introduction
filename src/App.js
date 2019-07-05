bc931871dee06edaa42e9c8206c1ee386c9692c9
BandarenkaDaniil <dbondarenko.post@gmail.com>
2019-07-05 15:12:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RailwayMap from './components/Map'
+
+class App extends React.Component {
+  render() {
+    return (
+        <RailwayMap/>
+    );
+  }
+}
+
+export default App;
